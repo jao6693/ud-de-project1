9d278fe58b92ef2f8cfe79ad4f84912018ddc831
Fabien Beuret <fabien.beuret@delaware.pro>
2020-04-27 13:56:24 +0200

    fix song_id + artists_id types

--- sql_queries.py
+++ sql_queries.py
@@ -10,17 +10,17 @@ time_table_drop = "DROP TABLE IF EXISTS d_time;"
 
 songplay_table_create = ("CREATE TABLE IF NOT EXISTS f_songplays \
   (songplay_id int, start_time timestamp, user_id int, \
-  level varchar(10), song_id int, artist_id int, \
+  level varchar(10), song_id varchar(18), artist_id varchar(18), \
   session_id int, location varchar(200), user_agent varchar(200));")
 
 user_table_create = ("CREATE TABLE IF NOT EXISTS d_users \
   (user_id int, first_name varchar(80), last_name varchar(100), gender character, level varchar(10));")
 
 song_table_create = ("CREATE TABLE IF NOT EXISTS d_songs \
-  (song_id int, title varchar(200), artist_id int, year smallint, duration smallint);")
+  (song_id varchar(18), title varchar(200), artist_id varchar(18), year smallint, duration numeric);")
 
 artist_table_create = ("CREATE TABLE IF NOT EXISTS d_artists \
-  (artist_id int, name varchar(200), location varchar(200), latitude int, longitude int);")
+  (artist_id varchar(18), name varchar(200), location varchar(200), latitude int, longitude int);")
 
 time_table_create = ("CREATE TABLE IF NOT EXISTS d_times \
   (start_time timestamp, hour smallint, day smallint, week smallint, month smallint, year smallint, weekday smallint);")
@@ -33,8 +33,9 @@ songplay_table_insert = ("""
 user_table_insert = ("""
 """)
 
-song_table_insert = ("""
-""")
+song_table_insert = ("INSERT INTO d_songs \
+  (song_id, title, artist_id, year, duration) \
+  VALUES(%s, %s, %s, %s, %s)")
 
 artist_table_insert = ("""
 """)
