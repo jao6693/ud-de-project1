f1a3871b1363ed87da1cd2921d2cbc660291a948
Fabien Beuret <fabien.beuret@delaware.pro>
2020-04-28 14:15:41 +0200

    update etl.py

--- etl.py
+++ etl.py
@@ -2,42 +2,59 @@ import os
 import glob
 import psycopg2
 import pandas as pd
+import numpy as np
+
 from sql_queries import *
 
 
 def process_song_file(cur, filepath):
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True).set_index('num_songs')
 
     # insert song record
-    song_data = 
+    song_data = df.loc[1, ['song_id', 'title', 'artist_id',
+                           'year', 'duration']].to_numpy().tolist()
+
+    # perform data type conversions
+    song_data = list(map(convert_np_to_native, song_data))
+
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = df.loc[1, ['artist_id', 'artist_name',
+                             'artist_location', 'artist_latitude', 'artist_longitude']]
+
+    artist_data = artist_data.fillna(0, inplace=True).to_numpy().tolist()
+    # perform data type conversions
+    artist_data = list(map(convert_np_to_native, artist_data))
+
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df.loc[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    t = 
+    t = pd.to_datetime(df.ts, unit='ms').drop_duplicates()
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = (t.loc[:], t.dt.hour, t.dt.day, t.dt.week,
+                 t.dt.month, t.dt.year, t.dt.weekday)
+    column_labels = ('start_time', 'hour', 'day',
+                     'week', 'month', 'year', 'weekday')
+    # https://stackoverflow.com/questions/18837262/convert-python-dict-into-a-dataframe
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df.loc[:, ['userId', 'firstName',
+                         'lastName', 'gender', 'level']].drop_duplicates()
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -46,17 +63,19 @@ def process_log_file(cur, filepath):
     # insert songplay records
     for index, row in df.iterrows():
         
-        # get songid and artistid from song and artist tables
+        # get song_id and artist_id from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
         
         if results:
-            songid, artistid = results
+            song_id, artist_id = results
         else:
-            songid, artistid = None, None
+            song_id, artist_id = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (str(pd.to_datetime(row.ts, unit='ms')), row.userId, row.level,
+                         song_id, artist_id, row.sessionId, row.location, row.userAgent)
+
         cur.execute(songplay_table_insert, songplay_data)
 
 
@@ -77,6 +96,18 @@ def process_data(cur, conn, filepath, func):
         func(cur, datafile)
         conn.commit()
         print('{}/{} files processed.'.format(i, num_files))
+
+
+def convert_np_to_native(value):
+    # convert numpy to native types to avoid problems during DB processing
+    if isinstance(value, np.int32):
+        return value.item()
+    if isinstance(value, np.int64):
+        return value.item()
+    if isinstance(value, np.float64):
+        return value.item()
+    # default
+    return value
 
 
 def main():
